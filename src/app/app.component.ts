70b35f7bfcc74c50c9be8bd8ea429a064d24cf2c
Mia Boyles <hdandmb@gmail.com>
2017-10-13 16:47:20 -0700

    date pipe added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {OnInit} from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
